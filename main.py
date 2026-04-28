40c8d4b8714c518791472def4676c6bb9e4b8ab2
dozapsixoza1 <166742425+dozapsixoza1@users.noreply.github.com>
2026-04-28 15:49:27 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
-from config import BOT_TOKEN
+from middlewares.ban import BanMiddleware
 from database.db import init_db
-from handlers import common, profile, duels, bosses, clans, shop, transfer, leaderboard
+from handlers import common, profile, duels, bosses, clans, shop, transfer, leaderboard, admin
 
 logging.basicConfig(level=logging.INFO)
 
@@ -25,7 +25,9 @@ async def main():
     dp.include_router(clans.router)
     dp.include_router(shop.router)
     dp.include_router(leaderboard.router)
+    dp.include_router(admin.router)
 
+    dp.message.middleware(BanMiddleware())
     await init_db()
     await dp.start_polling(bot)
 
